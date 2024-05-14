ea20ff49bf294ff84286ee6dc350b35c7df50a76
102693146 <abdullahsiddiqui44iq@users.noreply.github.com>
2024-05-14 20:21:41 +0500

    add quiz functionality

---
+++ src/quiz.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const authenticationModal = document.getElementById('authentication-modal');
+    authenticationModal.classList.remove('hidden');
+    authenticationModal.classList.add('flex');
+    authenticationModal.classList.add('justify-center');
+    authenticationModal.classList.add('items-center');
+    authenticationModal.classList.add('bg-gray-900');
+    authenticationModal.classList.add('bg-opacity-75');
+    
+
+});
